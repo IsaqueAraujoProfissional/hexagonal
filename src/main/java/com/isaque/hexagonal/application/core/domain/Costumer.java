a16c0ba321ddfce236529166d67469f6f51fcccb
Isaque Araujo <isaquearaujo.profissional@gmail.com>
2025-03-25 15:36:36 -0300

    creating the domain

---
+++ src/main/java/com/isaque/hexagonal/application/core/domain/Costumer.java
@@ -0,0 +1,73 @@
+package com.isaque.hexagonal.application.core.domain;
+
+import org.springframework.boot.autoconfigure.amqp.RabbitConnectionDetails.Address;
+
+public class Costumer {
+
+    public Costumer() {
+        this.isIsValidCpf = false;
+    }
+
+    public Costumer(String id, String name, Address address, String cpf, Boolean isValidCpf) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.cpf = cpf;
+        this.isValidCpf = isValidCpf;
+    }
+
+    private String id;
+
+    private String name;
+
+    private Address address;
+
+    private String cpf;
+
+    private Boolean isValidCpf;
+    
+    public String getId() {
+        return this.id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Address getAddress() {
+        return this.address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public String getCpf() {
+        return this.cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public Boolean isIsValidCpf() {
+        return this.isValidCpf;
+    }
+
+    public Boolean getIsValidCpf() {
+        return this.isValidCpf;
+    }
+
+    public void setIsValidCpf(Boolean isValidCpf) {
+        this.isValidCpf = isValidCpf;
+    }    
+    
+}
